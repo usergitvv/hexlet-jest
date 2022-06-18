b8a7a399d8674da34ccbecfdd25ac36c88ada3fe
usergitvv <valery.voronkov@yandex.ru>
2022-06-18 13:42:50 +0500

    Add teacher's decision (look at comment at down of file)

--- __tests__/fill.test.js
+++ __tests__/fill.test.js
@@ -33,3 +33,42 @@ test('fillFour', () => {
 test('fillEmpty', () => {    
     expect(fill([], '*')).toEqual([]);
 });
+
+/*
+Teacher's decision
+Обратить внимание на то, как учитель именует тестовые проверки!
+Именование должно отображать суть теста.
+
+// BEGIN
+let array;
+
+beforeEach(() => {
+  array = [1, 2, 3, 4];
+});
+
+test('common case', () => {
+  fill(array, '*', 1, 3);
+  expect(array).toEqual([1, '*', '*', 4]);
+});
+
+test('should use default start and end', () => {
+  fill(array, '*');
+  expect(array).toEqual(['*', '*', '*', '*']);
+});
+
+test('should works with start >= length', () => {
+  fill(array, '*', 10, 12);
+  expect(array).toEqual([1, 2, 3, 4]);
+});
+
+test('should works with start >= end', () => {
+  fill(array, '*', 2, 2);
+  expect(array).toEqual([1, 2, 3, 4]);
+});
+
+test('should works with end > length', () => {
+  fill(array, '*', 0, 10);
+  expect(array).toEqual(['*', '*', '*', '*']);
+});
+// END
+*/
